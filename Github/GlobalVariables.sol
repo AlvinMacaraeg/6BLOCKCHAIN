499293af71355bac1c50682f27f0bcf7745ec072
AlvinMacaraeg <alvinmacaraeg38@gmail.com>
2025-11-18 14:06:28 +0800

    Add files via upload

---
+++ Github/GlobalVariables.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract GlobalVariables {
+    function globalVars() external view returns (address, uint, uint){
+        address sender = msg.sender;
+        uint timestamp = block.timestamp;
+        uint blockNun = block.number;
+        return (sender, timestamp, blockNun);
+    }
+}
