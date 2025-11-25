6f163e3c5510db44bf919679ae9a2a2206a3b230
AlvinMacaraeg <alvinmacaraeg38@gmail.com>
2025-11-25 14:07:05 +0800

    Add files via upload

---
+++ GithubAct2/Constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^ 0.8.20;
+
+contract Constant {
+   address public constant my_address = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+   uint public  constant MY_UINT = 123;
+}
+
+contract Var {
+    address public MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+}
---
+++ GithubAct2/Ownable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^ 0.8.20;
+
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "Invalid Owner");
+        owner = _newOwner;
+    }
+
+    function onlyOwnerCanCallThisFunc() external onlyOwner {
+        // code
+    }
+
+    function anyonCanCall() external {
+        //code
+    }
+}
